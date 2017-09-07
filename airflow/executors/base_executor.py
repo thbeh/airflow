6a35b38c0bc4ac04b6b3e42cf42741288cdc1e50
grantnicholas <grant.nicholas@nielsen.com>
2017-09-07 12:04:36 -0500

    [AIRFLOW-1314] Use VolumeClaim for transporting DAGs

--- airflow/executors/base_executor.py
+++ airflow/executors/base_executor.py
@@ -40,7 +40,6 @@ class BaseExecutor(LoggingMixin):
         self.queued_tasks = {}
         self.running = {}
         self.event_buffer = {}
-        self.logger.setLevel(10)
 
     def start(self):  # pragma: no cover
         """
@@ -107,7 +106,7 @@ class BaseExecutor(LoggingMixin):
         """
         pass
 
-    def heartbeat(self, km=False):
+    def heartbeat(self):
         # Triggering new jobs
         if not self.parallelism:
             open_slots = len(self.queued_tasks)
@@ -133,7 +132,7 @@ class BaseExecutor(LoggingMixin):
             # does NOT eliminate it.
             self.queued_tasks.pop(key)
             ti.refresh_from_db()
-            if ti.state != State.RUNNING or km:
+            if ti.state != State.RUNNING:
                 self.running[key] = command
                 self.execute_async(key, command=command, queue=queue)
             else:

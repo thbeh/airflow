536f9eed22d150fce5b736f74ea32eeb21e742a3
Grant Nicholas <grantnicholas2015@u.northwestern.edu>
2017-09-21 17:26:51 -0500

    increase timeout on minikube

--- scripts/ci/kubernetes/kube/deploy.sh
+++ scripts/ci/kubernetes/kube/deploy.sh
@@ -27,7 +27,7 @@ kubectl apply -f $DIRNAME/postgres.yaml
 sed "s#{{docker_image}}#$IMAGE:$TAG#g" $DIRNAME/airflow.yaml.template > $DIRNAME/.generated/airflow.yaml && kubectl apply -f $DIRNAME/.generated/airflow.yaml
 
 
-# wait for up to 5 minutes for everything to be deployed
+# wait for up to 10 minutes for everything to be deployed
 for i in {1..150}
 do
   echo "------- Running kubectl get pods -------"
@@ -38,5 +38,5 @@ do
   if [ "$NUM_AIRFLOW_READY" == "1" ] && [ "$NUM_POSTGRES_READY" == "1" ]; then
     break
   fi
-  sleep 2
+  sleep 4
 done

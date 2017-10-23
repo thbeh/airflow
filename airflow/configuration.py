8135b5450046d8d978934a57f1dc0fbdfb995850
Benjamin Goldberg <benjigoldberg@users.noreply.github.com>
2017-10-23 18:45:50 -0500

    Update Airflow to Pass configuration to k8s containers, add some Py3 … (#9)
    * Update Airflow to Pass configuration to k8s containers, add some Py3 compat., create git-sync pod
    
    * Undo changes to display-source config setter for to_dict
    
    * WIP Secrets and Configmaps
    
    * Improve secrets support for multiple secrets. Add support for registry secrets. Add support for RBAC service accounts.
    
    * Swap order of variables, overlooked very basic issue
    
    * Secret env var names must be upper
    
    * Update logging
    
    * Revert spothero test code in setup.py
    
    * WIP Fix tests
    
    * Worker should be using local executor
    
    * Consolidate worker setup and address code review comments
    
    * reconfigure airflow script to use new secrets method

--- airflow/configuration.py
+++ airflow/configuration.py
@@ -7,9 +7,9 @@
 # to you under the Apache License, Version 2.0 (the
 # "License"); you may not use this file except in compliance
 # with the License.  You may obtain a copy of the License at
-# 
+#
 #   http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing,
 # software distributed under the License is distributed on an
 # "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -332,8 +332,8 @@ class AirflowConfigParser(ConfigParser):
                     {key.lower(): opt})
 
         # add bash commands
-        for (section, key) in self.as_command_stdout:
-            opt = self._get_cmd_option(section, key)
+        for (section, key) in AirflowConfigParser.as_command_stdout:
+            opt = self.get(section, key)
             if opt:
                 if not display_sensitive:
                     opt = '< hidden >'

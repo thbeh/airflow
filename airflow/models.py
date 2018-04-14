0f8e4451e7caa21c68739e092c7a35d11149be53
Jordan Zucker <jordan.zucker@gmail.com>
2018-04-13 17:02:21 -0700

    Oops, removed unnecessary broken log line

--- airflow/models.py
+++ airflow/models.py
@@ -5142,7 +5142,6 @@ class KubeResourceVersion(Base):
     @staticmethod
     @provide_session
     def reset_resource_version(session=None):
-        KubeResourceVersion.log.debug('Reset resource version')
         session.query(KubeResourceVersion).update({
             KubeResourceVersion.resource_version: '0'
         })

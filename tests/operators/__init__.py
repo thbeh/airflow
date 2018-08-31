6409d7850f5d0056c4dfe8e7ad3fc12dae3ae6df
Daniel Imberman <danielryan2430@gmail.com>
2018-08-31 14:19:42 -0700

    only import s3 tests if not in k8s mode

--- tests/operators/__init__.py
+++ tests/operators/__init__.py
@@ -7,21 +7,28 @@
 # to you under the Apache License, Version 2.0 (the
 # "License"); you may not use this file except in compliance
 # with the License.  You may obtain a copy of the License at
-# 
+#
 #   http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing,
 # software distributed under the License is distributed on an
 # "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-
+from subprocess import check_call
 from .docker_operator import *
 from .subdag_operator import *
 from .operators import *
 from .hive_operator import *
-from .s3_to_hive_operator import *
+try:
+    check_call(["kubectl", "get", "pods"])
+    from .s3_to_hive_operator import *
+except Exception as e:
+    raise unittest.SkipTest(
+        "Kubernetes integration tests require a minikube cluster;"
+        "Skipping tests {}".format(e)
+    )
 from .python_operator import *
 from .latest_only_operator import *
 
